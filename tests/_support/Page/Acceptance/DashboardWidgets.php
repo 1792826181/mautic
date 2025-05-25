e5270389217bc8a9137445045f99f34e59c69452
Sara Matos <sara.matos@tecnico.ulisboa.pt>
2025-05-25 13:13:51 +0100

    fix: refactor E2E tests

--- tests/_support/Page/Acceptance/DashboardWidgets.php
+++ tests/_support/Page/Acceptance/DashboardWidgets.php
@@ -9,4 +9,26 @@ class DashboardWidgets
     public static $ADMIN_USER       = 'admin';
     public static $ADMIN_PASSWORD   = 'Maut1cR0cks!';
     public static $URL              = '/s/dashboard';
+
+    public static $CONTACTS_COUNTER = 'Number of contacts';
+
+    public static $DNC_COUNTER      = 'Number of DNC contacts';
+
+    public static $CONTACTS_PER_SEGMENT = 'Number of contacts per segment';
+
+    public static $TOOLBAR = '#toolbar';
+
+    public static $FORM = 'form';
+
+    public static $ADD_WIDGET = 'Add widget';
+
+    public static $WIDGET_TYPE = '#widget_type_chosen';
+
+    public static $WIDGET_TYPE_CHOSEN = '#widget_type_chosen .chosen-single';
+
+    public static $WIDGET_TYPE_LIST = '//div[@id="widget_type_chosen"]//li';
+
+    public static $SAVE_BUTTON_TEXT = '//button[contains(., "Save")]';
+
+    public static $SAVE_BUTTON = 'button.btn-save';
 }
--- tests/_support/Step/Acceptance/DashboardStep.php
+++ tests/_support/Step/Acceptance/DashboardStep.php
@@ -17,39 +17,38 @@ class DashboardStep extends \AcceptanceTester
     private function createWidget(string $widgetType): void
     {
         $I = $this;
-        $I->waitForElementVisible('#toolbar', 10);
-        $I->click('Add widget', '#toolbar');
-        $I->waitForElementVisible('form', 10);
-        $I->waitForElementVisible('#widget_type_chosen', 10);
-        $I->waitForElementVisible('#widget_type_chosen .chosen-single', 10);
-        $I->click('#widget_type_chosen .chosen-single');
-        $I->click('//div[@id="widget_type_chosen"]//li[contains(text(), "'.$widgetType.'")]');
-        $I->waitForElementVisible('//button[contains(., "Save")]', 10);
-        $I->wait(1);
-        $I->executeJS("document.querySelector('button.btn-save').click();");
+        $I->waitForElementVisible(Dw::$TOOLBAR, 10);
+        $I->click(Dw::$ADD_WIDGET, Dw::$TOOLBAR);
+        $I->waitForElementVisible(Dw::$FORM, 10);
+        $I->waitForElementVisible(Dw::$WIDGET_TYPE, 10);
+        $I->waitForElementVisible(Dw::$WIDGET_TYPE_CHOSEN, 10);
+        $I->click(Dw::$WIDGET_TYPE_CHOSEN);
+        $I->click(Dw::$WIDGET_TYPE_LIST.'[contains(text(), "'.$widgetType.'")]');
+        $I->waitForElementVisible(Dw::$SAVE_BUTTON_TEXT, 10);
+        $I->executeJS("document.querySelector('".Dw::$SAVE_BUTTON."').click();");
     }
 
     public function createContactsCounterWidget(): void
     {
         $I = $this;
-        $I->createWidget('Number of contacts');
-        $I->waitForElementVisible('//h4[contains(text(), "Number of contacts")]', 10);
-        $I->see('Number of contacts');
+        $I->createWidget(Dw::$CONTACTS_COUNTER);
+        $I->waitForElementVisible('//h4[contains(text(), "'.Dw::$CONTACTS_COUNTER.'")]', 10);
+        $I->see(Dw::$CONTACTS_COUNTER);
     }
 
     public function createDncWidget(): void
     {
         $I = $this;
-        $I->createWidget('Number of DNC contacts');
-        $I->waitForElementVisible('//h4[contains(text(), "Number of DNC contacts")]', 10);
-        $I->see('Number of DNC contacts');
+        $I->createWidget(Dw::$DNC_COUNTER);
+        $I->waitForElementVisible('//h4[contains(text(), "'.Dw::$DNC_COUNTER.'")]', 10);
+        $I->see(Dw::$DNC_COUNTER);
     }
 
     public function createContactsCounterPerSegmentWidget(): void
     {
         $I = $this;
-        $I->createWidget('Number of contacts per segment');
-        $I->waitForElementVisible('//h4[contains(text(), "Number of contacts per segment")]', 10);
-        $I->see('Number of contacts per segment');
+        $I->createWidget(Dw::$CONTACTS_PER_SEGMENT);
+        $I->waitForElementVisible('//h4[contains(text(), "'.Dw::$CONTACTS_PER_SEGMENT.'")]', 10);
+        $I->see(Dw::$CONTACTS_PER_SEGMENT);
     }
 }
